bea745f0ba0618c7c2d55477a53af91c1df5eaac
Abdelwahab Ayman Samy AbuGhaleb <abdelwahab.aym.aboels3od@gmail.com>
2025-06-20 00:23:53 +0300

    Update 8.2 Problem.cpp

--- Lab 8-One Dimensional Arrays/8.2 Problem.cpp
+++ Lab 8-One Dimensional Arrays/8.2 Problem.cpp
@@ -1,1 +1,31 @@
+#include<iostream>
+using namespace std;
+const int max_size = 5;
+int arr[max_size]={};
+int count = 0;
+int main(){
+cout<<"Please Enter Five Different Numbers"<<endl;
+while(count< max_size&&cin>>arr[count]){
+    count++;
+}
+//while allow coditional execution of the code that will be exectutes as the codition is true
+//the condition is count less than array size which is 5 constant
+//by this i allowed to store my array element in count not max_size
+//the execution code is that count++
+cout<<"The array is diverse order is "<<endl;
+for (int i = count - 1; i>=0; i--){
+    cout<<arr[i];
+//for loop allow us to go through arrays element
+//each element is array++
+//subscrapbtin
+    if(i>0){
+        cout<<" ";
+    }if(count>0){
+        cout<<endl;
+    }
+}
+
+
+return 0;
+}
 
