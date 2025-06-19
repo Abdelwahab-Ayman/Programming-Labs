86ed77455bc4b7b22504070bc78d5dd8be78302b
Abdelwahab Ayman Samy AbuGhaleb <abdelwahab.aym.aboels3od@gmail.com>
2025-06-20 02:07:11 +0300

    Update 4.2.Problem.cpp

--- Lab 4-Conditional Statments/4.2.Problem.cpp
+++ Lab 4-Conditional Statments/4.2.Problem.cpp
@@ -1,1 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+int price,numbr1,number2,sum;
+cout<<"Please enter the unit price for first unit "<<"\n"<<"Please Enter the unit price for second unit"<<endl;
+cin>>numbr1>>number2;
+sum = numbr1 + number2;
+int discount = sum*.100;
+if(sum<1000){
+    cout<<"the amount is "<<sum<<endl;
+}else{
+    cout<<"your bill is "<<sum-discount<<endl;
+}
+return 0;
+}
+
 
