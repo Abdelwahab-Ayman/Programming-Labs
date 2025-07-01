b206162fa647f2e8f3b2fcf6beed1dfd6f746770
Abdelwahab Ayman Samy AbuGhaleb <abdelwahab.aym.aboels3od@gmail.com>
2025-07-01 14:41:15 +0300

    Update 7.cpp

--- Lab 7-Nested Looping Statments/7.cpp
+++ Lab 7-Nested Looping Statments/7.cpp
@@ -1,1 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int rows = 10;
+
+    for (int i = 1; i <= rows; ++i) {
+        int stars, spaces;
+
+        if (i <= 5) {
+            stars = 6 - i;
+            spaces = 2 * (i - 1);
+        } else {
+            stars = i - 5 + 1;
+            spaces = 2 * (rows - i);
+        }
+
+        // Print left stars
+        for (int j = 0; j < stars; ++j) {
+            cout << "*";
+        }
+
+        // Print spaces
+        for (int j = 0; j < spaces; ++j) {
+            cout << " ";
+        }
+
+        // Print right stars
+        for (int j = 0; j < stars; ++j) {
+            cout << "*";
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
 
