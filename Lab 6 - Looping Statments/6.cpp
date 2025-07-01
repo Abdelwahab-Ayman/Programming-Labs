a2d29efb4261d4c0c5a77c6f2425edf32f43d83a
Abdelwahab Ayman Samy AbuGhaleb <abdelwahab.aym.aboels3od@gmail.com>
2025-07-01 12:06:12 +0300

    Update 6.cpp

--- Lab 6 - Looping Statments/6.cpp
+++ Lab 6 - Looping Statments/6.cpp
@@ -1,1 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+char sym = '*';
+int i = 0;
+while( i < 6){
+    cout<<sym;
+    i++;
+}
+for(int i =0; i<6; i++){
+    cout<<sym<<" "<<endl;
+}
 
+return 0;
+}
